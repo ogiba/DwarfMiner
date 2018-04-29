5486f6e4872806c02feda2ac6fbb8afdde1080f6
Robert Ogiba <ogiback@gmail.com>
2018-04-29 23:48:43 +0200

    Few changes in app layout

--- src/main/java/pl/ogiba/dwarf/MainApp.java
+++ src/main/java/pl/ogiba/dwarf/MainApp.java
@@ -2,12 +2,16 @@ package pl.ogiba.dwarf;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.geometry.HPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -19,22 +23,31 @@ public class MainApp extends Application {
 
         TreeView<String> treeView = setupTree();
 
-        StackPane root = new StackPane();
-        root.getChildren().add(treeView);
+        ScrollPane root = new ScrollPane();
+        root.setContent(treeView);
 
         GridPane gridPane = new GridPane();
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+        ColumnConstraints column1 = new ColumnConstraints(100);
+        ColumnConstraints column2 = new ColumnConstraints(50, 150, 300);
+        column2.setHgrow(Priority.ALWAYS);
+        gridPane.getColumnConstraints().addAll(column1, column2);
 
         TextArea textArea = new TextArea();
         Button button = new Button();
+        button.setMinSize(100, 50);
+        GridPane.setHalignment(button, HPos.RIGHT);
+
         button.setText("Insert");
 
-        GridPane.setFillWidth(button, true);
-        
-        gridPane.add(root, 0, 0);
-        gridPane.add(textArea, 1,0);
-        gridPane.add(button, 0, 1);
+        gridPane.add(root, 0, 0, 2, 1);
+        gridPane.add(textArea, 2, 0, 2, 1);
+        gridPane.add(button, 1, 1, 2, 1);
 
-        Scene scene = new Scene(gridPane, 300, 300);
+        GridPane.setFillWidth(button, true);
+
+        Scene scene = new Scene(gridPane, 500, 500);
 //        scene.getStylesheets().add("/styles/Styles.css");
 
         stage.setTitle("JavaFX and Maven");
