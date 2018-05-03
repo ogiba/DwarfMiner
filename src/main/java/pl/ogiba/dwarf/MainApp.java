2a527fbb1d8b195a9930952811a7e4c8c050dba7
Robert Ogiba <robert.ogiba@outlook.com>
2018-05-03 21:56:58 +0200

    Provide simple TextArea that will be used to inserting data

--- src/main/java/pl/ogiba/dwarf/MainApp.java
+++ src/main/java/pl/ogiba/dwarf/MainApp.java
@@ -10,9 +10,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -28,71 +30,63 @@ public class MainApp extends Application {
 
         MongoDatabase db = mongoClient.getDatabase("dwarf_test");
 
-//        TreeView<String> treeView = setupTree();
-//
-//        ScrollPane root = new ScrollPane();
-//        root.setContent(treeView);
-//
-//        GridPane gridPane = new GridPane();
-//        gridPane.setHgap(5);
-//        gridPane.setVgap(5);
-//        ColumnConstraints column1 = new ColumnConstraints(100);
-//        ColumnConstraints column2 = new ColumnConstraints(50, 150, 300);
-//        column2.setHgrow(Priority.ALWAYS);
-//        gridPane.getColumnConstraints().addAll(column1, column2);
-//
-//        TextArea textArea = new TextArea();
+
 //        Button button = new Button();
 //        button.setMinSize(100, 50);
-//        GridPane.setHalignment(button, HPos.RIGHT);
 //
 //        button.setText("Insert");
 //
-//        gridPane.add(root, 0, 0, 2, 1);
-//        gridPane.add(textArea, 2, 0, 2, 1);
-//        gridPane.add(button, 1, 1, 2, 1);
 //
-//        GridPane.setFillWidth(button, true);
-//
-//        Scene scene = new Scene(gridPane, 500, 500);
-////        scene.getStylesheets().add("/styles/Styles.css");
-//
-//        stage.setTitle("JavaFX and Maven");
-//        stage.setScene(scene);
-//        stage.show();
         HBox hbox = new HBox(20);
         hbox.setTranslateX(20);
         hbox.setTranslateY(20);
-        
-        
 
         SplitPane splitPane = new SplitPane();
         splitPane.autosize();
         splitPane.setOrientation(Orientation.HORIZONTAL);
-//        splitPane1.setPrefSize(200, 200);
-        final Button l1 = new Button("Left Button");
+
         final Button r1 = new Button("Right Button");
+
+        TreeView nodesTree = setupTree();
+
+//        ScrollPane scrollPane = new ScrollPane(nodesTree);
+        AnchorPane nodesContainer = new AnchorPane();
+
+        AnchorPane.setLeftAnchor(nodesTree, 0.0);
+        AnchorPane.setRightAnchor(nodesTree, 0.0);
+        AnchorPane.setTopAnchor(nodesTree, 0.0);
+        AnchorPane.setBottomAnchor(nodesTree, 0.0);
+
+        nodesContainer.getChildren().add(nodesTree);
         
-        ScrollPane scrollPane = new ScrollPane(l1);
-//        BorderPane borderPane = new BorderPane(l1);
-        BorderPane borderPane2 = new BorderPane(r1);
+        TextArea dataArea = setupTextArea();
         
-        splitPane.getItems().addAll(scrollPane, borderPane2);
-        hbox.getChildren().add(splitPane);
+        AnchorPane dataContainer = new AnchorPane();
         
+        AnchorPane.setLeftAnchor(dataArea, 0.0);
+        AnchorPane.setRightAnchor(dataArea, 0.0);
+        AnchorPane.setTopAnchor(dataArea, 0.0);
+        AnchorPane.setBottomAnchor(dataArea, 0.0);
+        
+        dataContainer.getChildren().add(dataArea);
+
+        splitPane.getItems().addAll(nodesContainer, dataContainer);
+
         AnchorPane anchorPane = new AnchorPane();
-        
+
         AnchorPane.setLeftAnchor(splitPane, 10.0);
         AnchorPane.setRightAnchor(splitPane, 10.0);
         AnchorPane.setTopAnchor(splitPane, 10.0);
         AnchorPane.setBottomAnchor(splitPane, 10.0);
-        
+
         anchorPane.getChildren().add(splitPane);
 
-        Scene scene = new Scene(anchorPane, 560, 240);
+        Scene scene = new Scene(anchorPane, 640, 480);
+        scene.getStylesheets().add("/styles/Styles.css");
         scene.setFill(Color.GHOSTWHITE);
-        stage.setScene(scene);
+        
         stage.setTitle("SplitPane");
+        stage.setScene(scene);
         stage.show();
     }
 
@@ -110,6 +104,11 @@ public class MainApp extends Application {
         nodeItemA.getChildren().addAll(nodeItemA1, nodeItemA2, nodeItemA3);
 
         return new TreeView<>(treeItemRoot);
+    }
+    
+    private TextArea setupTextArea() {
+          final TextArea textArea = new TextArea();
+          return textArea;
     }
 
     /**
