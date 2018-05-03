467e2224309401538da37a83b5e3aadde3edfd74
Robert Ogiba <robert.ogiba@outlook.com>
2018-05-03 21:20:02 +0200

    Add dependecy for MongoDB Driver. Play with Split and Anchro pane

--- src/main/java/pl/ogiba/dwarf/MainApp.java
+++ src/main/java/pl/ogiba/dwarf/MainApp.java
@@ -1,18 +1,22 @@
 package pl.ogiba.dwarf;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -20,38 +24,75 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 //        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
 
-        TreeView<String> treeView = setupTree();
+        MongoDatabase db = mongoClient.getDatabase("dwarf_test");
 
-        ScrollPane root = new ScrollPane();
-        root.setContent(treeView);
+//        TreeView<String> treeView = setupTree();
+//
+//        ScrollPane root = new ScrollPane();
+//        root.setContent(treeView);
+//
+//        GridPane gridPane = new GridPane();
+//        gridPane.setHgap(5);
+//        gridPane.setVgap(5);
+//        ColumnConstraints column1 = new ColumnConstraints(100);
+//        ColumnConstraints column2 = new ColumnConstraints(50, 150, 300);
+//        column2.setHgrow(Priority.ALWAYS);
+//        gridPane.getColumnConstraints().addAll(column1, column2);
+//
+//        TextArea textArea = new TextArea();
+//        Button button = new Button();
+//        button.setMinSize(100, 50);
+//        GridPane.setHalignment(button, HPos.RIGHT);
+//
+//        button.setText("Insert");
+//
+//        gridPane.add(root, 0, 0, 2, 1);
+//        gridPane.add(textArea, 2, 0, 2, 1);
+//        gridPane.add(button, 1, 1, 2, 1);
+//
+//        GridPane.setFillWidth(button, true);
+//
+//        Scene scene = new Scene(gridPane, 500, 500);
+////        scene.getStylesheets().add("/styles/Styles.css");
+//
+//        stage.setTitle("JavaFX and Maven");
+//        stage.setScene(scene);
+//        stage.show();
+        HBox hbox = new HBox(20);
+        hbox.setTranslateX(20);
+        hbox.setTranslateY(20);
+        
+        
 
-        GridPane gridPane = new GridPane();
-        gridPane.setHgap(5);
-        gridPane.setVgap(5);
-        ColumnConstraints column1 = new ColumnConstraints(100);
-        ColumnConstraints column2 = new ColumnConstraints(50, 150, 300);
-        column2.setHgrow(Priority.ALWAYS);
-        gridPane.getColumnConstraints().addAll(column1, column2);
+        SplitPane splitPane = new SplitPane();
+        splitPane.autosize();
+        splitPane.setOrientation(Orientation.HORIZONTAL);
+//        splitPane1.setPrefSize(200, 200);
+        final Button l1 = new Button("Left Button");
+        final Button r1 = new Button("Right Button");
+        
+        ScrollPane scrollPane = new ScrollPane(l1);
+//        BorderPane borderPane = new BorderPane(l1);
+        BorderPane borderPane2 = new BorderPane(r1);
+        
+        splitPane.getItems().addAll(scrollPane, borderPane2);
+        hbox.getChildren().add(splitPane);
+        
+        AnchorPane anchorPane = new AnchorPane();
+        
+        AnchorPane.setLeftAnchor(splitPane, 10.0);
+        AnchorPane.setRightAnchor(splitPane, 10.0);
+        AnchorPane.setTopAnchor(splitPane, 10.0);
+        AnchorPane.setBottomAnchor(splitPane, 10.0);
+        
+        anchorPane.getChildren().add(splitPane);
 
-        TextArea textArea = new TextArea();
-        Button button = new Button();
-        button.setMinSize(100, 50);
-        GridPane.setHalignment(button, HPos.RIGHT);
-
-        button.setText("Insert");
-
-        gridPane.add(root, 0, 0, 2, 1);
-        gridPane.add(textArea, 2, 0, 2, 1);
-        gridPane.add(button, 1, 1, 2, 1);
-
-        GridPane.setFillWidth(button, true);
-
-        Scene scene = new Scene(gridPane, 500, 500);
-//        scene.getStylesheets().add("/styles/Styles.css");
-
-        stage.setTitle("JavaFX and Maven");
+        Scene scene = new Scene(anchorPane, 560, 240);
+        scene.setFill(Color.GHOSTWHITE);
         stage.setScene(scene);
+        stage.setTitle("SplitPane");
         stage.show();
     }
 
