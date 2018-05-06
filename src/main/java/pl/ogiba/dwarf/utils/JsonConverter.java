9fb30af405a4a4c492f0799e52b03c7a00a0a976
Robert Ogiba <ogiback@gmail.com>
2018-05-06 22:55:47 +0200

    Replace temporarily (or maybe not) regex with gson pretty printing

--- src/main/java/pl/ogiba/dwarf/utils/JsonConverter.java
+++ src/main/java/pl/ogiba/dwarf/utils/JsonConverter.java
@@ -5,8 +5,12 @@
  */
 package pl.ogiba.dwarf.utils;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.util.StringConverter;
 
 /**
@@ -26,25 +30,53 @@ public class JsonConverter extends StringConverter<String> {
 
     @Override
     public String fromString(String string) {
-        Pattern regex = Pattern.compile("[\\[\\{*,\\}\\]]");
+//        Pattern regex = Pattern.compile("[\\[\\{*,\\}\\]]");
+//        String regex = "[\\[\\{*,\\}\\]]";
 
-        String formattedValue = "";
-        for (char singleChar : string.toCharArray()) {
-            String currentChar = String.valueOf(singleChar);
-
-            final Matcher matcher = regex.matcher(currentChar);
-
-            if (matcher.find()) {
-                if (currentChar.equals("}") || currentChar.equals("]")) {
-                    formattedValue += "\n" + currentChar;
-                } else {
-                    formattedValue += currentChar + "\n";
-                }
-            } else {
-                formattedValue += currentChar;
-            }
-        }
+//        String openRegex = "[\\[\\{,]";
+//        String openReplacement = "$0\n";
+//
+//        String formattedValue = string.replaceAll(openRegex, openReplacement);
+//
+//        String closeRegex = "[\\]\\}]";
+//        String closeReplacement = "\n$0";
+        String formattedValue = toPrettyFormat(string);//formattedValue.replaceAll(closeRegex, closeReplacement);
+        //        for (char singleChar : string.toCharArray()) {
+        //            String currentChar = String.valueOf(singleChar);
+        //
+        //            final Matcher matcher = regex.matcher(currentChar);
+        //
+        //            if (matcher.find()) {
+        //                if (currentChar.equals("}") || currentChar.equals("]")) {
+        //                    formattedValue += "\n" + currentChar;
+        //                } else {
+        //                    formattedValue += currentChar + "\n";
+        //                }
+        //            } else {
+        //                formattedValue += currentChar;
+        //            }
+        //        }
         return formattedValue;
+    }
+
+    public String toPrettyFormat(String jsonString) {
+        JsonParser parser = new JsonParser();
+        JsonElement jsonElement = parser.parse(jsonString);
+        
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        String prettyJson;
+        if (jsonElement.isJsonArray()) {
+            JsonArray json = jsonElement.getAsJsonArray();
+
+            prettyJson = gson.toJson(json);
+        } else {
+            JsonObject json = jsonElement.getAsJsonObject();
+
+            prettyJson = gson.toJson(json);
+        }
+
+        return prettyJson;
     }
 
 }
