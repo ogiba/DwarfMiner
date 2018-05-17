7c39fc0549f82abfffe6b659f961ee0d0e7ee1d8
Robert Ogiba <ogiback@gmail.com>
2018-05-17 19:36:26 +0200

    Provide method that using simple statements checks data to execute right action

--- src/main/java/pl/ogiba/dwarf/scenes/insert/InsertDocumentPresenter.java
+++ src/main/java/pl/ogiba/dwarf/scenes/insert/InsertDocumentPresenter.java
@@ -5,29 +5,56 @@
  */
 package pl.ogiba.dwarf.scenes.insert;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import java.io.Console;
+import com.mongodb.util.JSON;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.bson.Document;
-import sun.rmi.runtime.Log;
 
 /**
  *
  * @author robertogiba
  */
-public class InsertDocumentPresenter implements IInsertDocumentPresenter{
+public class InsertDocumentPresenter implements IInsertDocumentPresenter {
+
     private MongoCollection<Document> mongoCollection;
 
     @Override
     public void transferCollectionReference(MongoCollection<Document> collection) {
         this.mongoCollection = collection;
-    }  
-  
+    }
+
     @Override
     public void insertDataToDb(String data) {
-        System.out.println(data);
-        
+        convertJsonString(data);
+    }
+
+    private void convertJsonString(String data) {
+        final String dataToCheck = data.trim();
+
+        if (dataToCheck.startsWith("{") && dataToCheck.endsWith("}")) {
+            prepareDocumentForData(data);
+        } else if (dataToCheck.startsWith("[") && dataToCheck.endsWith("]")) {
+            prepareDocumentListForData(data);
+        } else {
+            System.out.println("Data has invalid format");
+        }
+    }
+
+    private void prepareDocumentForData(String data) {
         Document document = Document.parse(data);
         System.out.println(document);
+    }
+    
+     private void prepareDocumentListForData(String data) {
+         BasicDBList parsedData = (BasicDBList)JSON.parse(data);
+         
+        final List<Document> properData = parsedData.stream().map((doc) -> { 
+            return Document.parse(doc.toString());
+        }).collect(Collectors.toList());
+        System.out.println("Data is json array");
     }
 }
